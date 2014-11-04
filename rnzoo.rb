f2e565eff7efda26a40939a5e9c1e7fd764094eb
reiki4040 <reiki4040@gmail.com>
2014-11-04 23:21:12 +0900

    version 0.2.1

--- rnzoo.rb
+++ rnzoo.rb
@@ -1,23 +1,24 @@
 require "formula"
 
-HOMEBREW_RNZOO_VERSION="0.2.0"
+HOMEBREW_RNZOO_VERSION="0.2.1"
+
 class Rnzoo < Formula
   homepage 'https://github.com/reiki4040/rnzoo'
 
   if OS.mac?
     url "https://github.com/reiki4040/rnzoo/releases/download/#{HOMEBREW_RNZOO_VERSION}/rnzoo-#{HOMEBREW_RNZOO_VERSION}-darwin-amd64.zip"
-    sha1 '203853d469537d925d90a25bdb9112e7e395b0ec'
+    sha1 '40fb17013cff0b642ef8eafe506a878b4607d19f'
   end
 
   version=HOMEBREW_RNZOO_VERSION
 
   depends_on 'peco'
-  
+
   def install
     # rnssh, ec2list, ltsv_pipe
     bin.install Dir['bin/*']
   end
- 
+
   # show message after installation.
   def caveats
     msg = <<-EOF.undent
@@ -27,7 +28,7 @@ class Rnzoo < Formula
 
     export AWS_ACCESS_KEY_ID="YOUR AWS ACCESS KEY"
     export AWS_SECRET_ACCESS_KEY="YOUR AWS SECRET ACCESS KEY"
-    
+
     # Option: you can set default aws region.
 
     export AWS_REGION="ap-northeast-1"
