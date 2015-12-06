fe24018a2bf9e1dbcb07fe26f275ea58ba70ab4f
reiki4040 <reiki4040@gmail.com>
2015-12-06 21:43:30 +0900

    updated rnzoo to 0.3.2

--- rnzoo.rb
+++ rnzoo.rb
@@ -3,11 +3,11 @@ require "formula"
 class Rnzoo < Formula
   desc "useful tool for EC2."
   homepage 'https://github.com/reiki4040/rnzoo'
-  version "0.3.1"
+  version "0.3.2"
 
   if OS.mac?
     url "https://github.com/reiki4040/rnzoo/releases/download/v" + version + "/rnzoo-" + version + "-darwin-amd64.zip"
-    sha256 'bf5dbfc60c1ae922151cad5ee92cebd7c4f1cc70fea4b85972b01bc6d273cbac'
+    sha256 'bc87dc44771df84d4043285c3d988d0123a4d64a16d16c1b9fb9f658f0f35841'
   end
 
   def install
