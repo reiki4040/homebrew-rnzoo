070ff5ad9bffab993c6cdfc7947ccbd3e1b6257c
reiki4040 <reiki4040@gmail.com>
2017-02-11 23:57:37 +0900

    quick fix formula strict.
    resolved below command output.
    brew audit --strict --online rnzoo.rb

--- rnzoo.rb
+++ rnzoo.rb
@@ -1,22 +1,20 @@
-require "formula"
-
 class Rnzoo < Formula
   desc "useful tool for EC2."
-  homepage 'https://github.com/reiki4040/rnzoo'
+  homepage "https://github.com/reiki4040/rnzoo"
   version "0.4.1"
 
   if OS.mac?
     url "https://github.com/reiki4040/rnzoo/releases/download/v" + version + "/rnzoo-v" + version + "-darwin-amd64.tar.gz"
-    sha256 'ae6d03019357c30fa087fe75ebcaaf2275cfab108afcc8dae4fc0a880ad9e9e2'
+    sha256 "ae6d03019357c30fa087fe75ebcaaf2275cfab108afcc8dae4fc0a880ad9e9e2"
   end
 
   def install
-    bin.install 'rnzoo'
+    bin.install "rnzoo"
   end
 
   # show message after installation.
   def caveats
-    msg = <<-EOF.undent
+    <<-EOF.undent
     # rnzoo need AWS access key for working.
     # Please set key/secret to AWS credentials (~/.aws/credentials)
 
@@ -35,5 +33,10 @@ class Rnzoo < Formula
     rnzoo init
 
 EOF
+  end
+
+  # test
+  test do
+    # nothing
   end
 end
